fa1256b2eb5ea73a22357903082c50978367e163
Alexander Ignatiev <alexandr.ignatev@gmail.com>
2021-03-30 18:01:32 +0300

    fix ValidationError handle, class name for booking tests and impove .gitignore

--- tests/booking/test_get_booking_ids.py
+++ tests/booking/test_get_booking_ids.py
@@ -2,6 +2,7 @@ from requests.models import Response
 from booking_service import Booking
 
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 
 def test_get_booking_ids_should_return_booking_ids(base_url: str):
@@ -20,4 +21,7 @@ def test_get_booking_ids_should_return_booking_ids(base_url: str):
     resp: Response = Booking(base_url).get_booking_ids()
 
     assert resp.status_code == 200
-    validate(resp.json(), schema)
+    try:
+        validate(resp.json(), schema)
+    except ValidationError as e:
+        assert False, e.message
